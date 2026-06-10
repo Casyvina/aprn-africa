85815727ac7e190c04f66160c0c86f466de1cdf4
Joseph Agwuh <josephagwuh@gmail.com>
2026-06-10 05:41:45 +0100

    chore: ignore website-templates dir, fix write-client env var name

--- lib/sanity/write-client.ts
+++ lib/sanity/write-client.ts
@@ -6,5 +6,5 @@ export const writeClient = createClient({
   dataset:   process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   apiVersion: '2025-05-01',
   useCdn: false,
-  token: process.env.SANITY_API_WRITE_TOKEN,
+  token: process.env.SANITY_WRITE_TOKEN,
 })
